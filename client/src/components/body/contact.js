0c598e166881cb3bd74a7c1922c48af0bf3414c9
adiXcodr <ichbinadittyadey@gmail.com>
2020-06-13 21:37:24 +0530

    first commit

--- client/src/components/body/contact.js
+++ client/src/components/body/contact.js
@@ -21,9 +21,9 @@ class Contact extends React.Component {
       
                     <p class="contact_box_head">CONTACT US</p>
                     <form action="https://formspree.io/xrgykobq" method="POST" class="cf">
-                      <input type="text" id="input-name" placeholder="Name*" required/>
-                      <input type="email" id="input-email" placeholder="Email address*" required/>
-                      <input type="text" id="input-subject" placeholder="Subject (Optional)" />
+                      <input name="name" type="text" id="input-name" placeholder="Name*" required/>
+                      <input name="email" type="email" id="input-email" placeholder="Email address*" required/>
+                      <input name="subject" type="text" id="input-subject" placeholder="Subject (Optional)" />
                       <textarea name="message" type="text" id="input-message" placeholder="Message (Optional)"></textarea>
 
                     <input type="submit" value="Submit" id="input-submit"/>
