fe4003fbccc789eb61f174fbe157c9d003250152
adiXcodr <ichbinadittyadey@gmail.com>
2020-06-14 15:42:31 +0530

    fixes

--- client/src/components/body/home.js
+++ client/src/components/body/home.js
@@ -67,15 +67,9 @@ class Home extends Component {
         <div className="about row">
 
           <div class="aboutieee col-auto">
-<<<<<<< HEAD
-            <div className="header">ABOUT NSS Tezpur University</div>
-            <div className="text">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec auctor tincidunt dictum. Fusce aliquam convallis viverra. Quisque iaculis odio id neque vulputate, a sollicitudin orci lacinia. Duis ut ultricies orci. Maecenas ac blandit lorem. Ut sollicitudin turpis ante, a lobortis diam condimentum vitae. Nulla aliquam erat mauris, in volutpat ex eleifend ac. Donec facilisis, elit id aliquam efficitur, ex tellus scelerisque tellus, a cursus sem ipsum sit amet ligula.
-=======
             <div className="header">NSS Tezpur University</div>
             <div className="text">
             National Service Scheme Tezpur University, popularly known as NSS is an extension dimension to the higher education system to orient the student youth to community service while they are studying in educational institutions, under the aegis of Ministry of Youth Affairs & Sports, Govt. of India.
->>>>>>> 88dcc628e401042751904c3f69ce716729a56d2e
               <br />{" "}
               <a
                 href="http://www.tezu.ernet.in/"
@@ -102,10 +96,6 @@ class Home extends Component {
                         <li>{this.showNotifications(this.props.notifications)}</li>
                       </ul>
                     </marquee>
-<<<<<<< HEAD
-        </div>
-        <div className="about row">
-=======
         </div>  
 
            
@@ -128,26 +118,8 @@ class Home extends Component {
           </div>
 
          
->>>>>>> 88dcc628e401042751904c3f69ce716729a56d2e
 
-          <div class="aboutnss col-auto">
-            <div className="header">ABOUT NSS Tezpur University</div>
-            <div className="text">
-              The National Service Scheme (NSS) is a Central Sector Scheme of Government of India, Ministry of Youth Affairs & Sports. It provides opportunity to the student youth of 11th & 12th Class of schools at +2 Board level and student youth of Technical Institution, Graduate & Post Graduate at colleges and University level of India to take part in various government led community service activities & programmes.The sole aim of the NSS is to provide hands on experience to young students in delivering community service. 
-              <br />{" "}
-              <a
-                href="https://nss.gov.in/"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Visit NSS
-              </a>
-            </div>
-          </div>
-
-        </div> 
-
-        </div>  {/*Row ends here*/}
+        
 
       </div>
     );
